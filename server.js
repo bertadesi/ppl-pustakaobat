d88032eb8a2231e538dd5cf17c3afeb2f62fdea5
bertadesi <88239561+bertadesi@users.noreply.github.com>
2023-08-15 13:00:39 +0700

    Change query and db connectivity local

--- server.js
+++ server.js
@@ -2,23 +2,41 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Machine learning model
+// Create a RandomForest model
+
 
 app.get('/message', (req, res) => {
   res.json({ message: "Pustaka Obat" });
 });
 
 const connection = mysql.createConnection({
-  host: 'sql6.freemysqlhosting.net',
-  user: 'sql6637284',
-  password: '62VZuthkR5',
-  database: 'sql6637284',
+  host: 'localhost',
+  user: 'root',
+  password: 'admin123#',
+  database: 'pustakaobat',
 });
 
 app.get('/data', (req, res) => {
-  connection.query('SELECT * FROM penyakit a, Obat b where a.id = b.id_penyakit', (error, results) => {
+
+const query = `
+    SELECT namaPenyakit, DeskripsiGejala, idObat, namaObat, dosis, tipe, '' as namaDokter, '' as klinik 
+    FROM penyakit a, obat b 
+    WHERE a.idpenyakit = b.idpenyakit
+    UNION ALL
+    SELECT namaPenyakit, DeskripsiGejala, b.idObat, namaObat, dosis, b.tipe, d.nama as namaDokter, d.klinik as klinik 
+    FROM resep a, obat b, pemeriksaan c, dokter d, penyakit e
+    WHERE a.idObat = b.idObat
+    AND b.idPenyakit = c.idPenyakit
+    AND c.idDokter = d.idDokter
+    AND e.idPenyakit = c.idPenyakit
+  `;	
+  connection.query(query, (error, results) => {
     if (error) {
       console.error(error);
       return res.status(500).json({ error: 'Database error' });
@@ -27,6 +45,8 @@ app.get('/data', (req, res) => {
     return res.json(results);
   });
 });
+
+
 
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
