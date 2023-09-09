2942a13582b86e580ba1e3234f292521354acef3
bertadesi <88239561+bertadesi@users.noreply.github.com>
2023-09-09 12:23:26 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,166 @@
-from flask import Flask, request, render_template
-from rdflib import Graph
-from rdflib.plugins.sparql import prepareQuery
-
-app = Flask(__name__)
-
-g = Graph()
-try:
-    g.parse("sample.rdf", format="xml")
-except Exception as e:
-    print("Error parsing RDF data:", e)
+import React, { useState, useEffect, useMemo } from "react";
+import "./App.css";
 
 
 
-@app.route('/query', methods=['GET'])
-def query_rdf():
-    query_string = request.args.get('q')  # Get the 'q' parameter from the query string
-    query = prepareQuery(query_string)
+function App() {
+  const [message, setMessage] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [items, setItems] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [showSearchResults, setShowSearchResults] = useState(false);
+  const [showsearchQuery, setShowSearchQuery] = useState(false);
 
-    results = g.query(query)
-    print(results)
-    results_list = []
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error(error));
 
-    for row in results:
-        result_dict = {
-            'name': row['name'],
-            'hobby': row['hobby']
-        }
-        results_list.append(result_dict)
+    fetch("http://localhost:8000/data")
+      .then((response) => response.json())
+      .then((data) => {
+        // Filter out duplicate items by comparing the 'id' property
+        //const uniqueItems = data.filter(
+       //   (item, index, self) => index === self.findIndex((i) => i.id === item.id)
+     //   );
+        const uniqueItems = data
 
-    return render_template('index.html', results=results_list)
+        setItems(uniqueItems);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    setShowSearchQuery(true);
+  };
+
+  const handleSearchPrediction = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/searchObat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: searchQuery }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setSearchResults(data.prediction);
+        setShowSearchResults(true);
+      } else {
+        console.error('Failed to fetch data:', response.status);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  const filteredItems = useMemo(() => {
+    if (!items || items.length === 0) {
+    return [];
+  }
+    return items.filter((item) =>
+    item.namaPenyakit.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  }, [items, searchTerm]);
 
 
 
+  // New list of resulted item IDs
+  const resultedItemIds = filteredItems.map((item) => item.Id);
 
+  
+  return (
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    <div style={{ backgroundImage: "url(/obat.PNG)", backgroundSize: 'cover'
+ }}>
+      <title>W3.CSS</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3pro.css" />
+
+      <div className="w3-container w3-card"   >
+        <h1 style= {{background: '#d15890', color: '#edf7f7',textAlign: 'center'}}>{message}</h1>
+      </div>
+
+      <div className="w3-container w3-card">
+        <h2 style={{ color: '#edf7f7', background: '#d15890'}} > Daftar Penyakit</h2>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+
+        <ul className="w3-ul w3-card">
+  {Array.from(new Set(filteredItems.map((item) => item.namaPenyakit))).map((namaPenyakit) => {
+    const uniqueItem = filteredItems.find((item) => item.namaPenyakit === namaPenyakit);
+    return (
+      <li key={uniqueItem.id} style={{ border: '1px solid #ccc', marginBottom: '10px', padding: '10px' }}>
+        <div className="item-container">
+          <img src={uniqueItem.thumbnailUrl} alt={uniqueItem.name} className="thumbnail" />
+          <div>
+            <h3>{uniqueItem.namaPenyakit}</h3>
+            <p>{uniqueItem.DeskripsiGejala}</p>
+          </div>
+        </div>
+      </li>
+    );
+  })}
+</ul>
+</div>
+{showsearchQuery && (
+<div className="w3-container w3-card">
+  <h2 style={{ color: '#edf7f7', background: '#d15890'}}>Daftar Obat</h2>
+  <ul className="w3-ul w3-card">
+    {Array.from(new Set(filteredItems.map((item) => item.namaPenyakit))).map((namaPenyakit) => {
+      const uniqueItems = filteredItems.filter((item) => item.namaPenyakit === namaPenyakit);
+      return (
+        <li key={uniqueItems[0].id}>
+          <div className="item-container">
+            <h3>{namaPenyakit}</h3>
+            {uniqueItems.map((item) => (
+              <div key={item.idObat}>
+               
+                <p>
+                ID: {item.idObat}, Nama Obat: {item['namaObat']}, Ukuran: {item.dosis}, tipe: {item.tipe}
+                {item.namaDokter ? <span className="sparkling-text"><span style={{ color: 'purple' }}> diresepkan oleh: {item.namaDokter}</span> </span> : ''}
+                 {item.klinik ? <span className="sparkling-text"><span style={{ color: 'purple' }}>klinik: {item.klinik}</span></span> : ''}
+                </p>
+
+              </div>
+            ))}
+          </div>
+        </li>
+      );
+    })}
+  </ul>
+  </div>)}
+  <div className="w3-container w3-card">
+      <h2 style={{ color: '#edf7f7', background: '#d15890'}}>Cek Rekomendasi</h2>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button onClick={handleSearchPrediction}>Cari</button>
+       <div>
+        {showSearchResults && ( // Conditionally render search results
+          <div>
+            <h2 style={{ color: '#edf7f7', background: '#d15890'}}>Hasil</h2>
+            <ul>
+              {searchResults.map((result, index) => (
+                <li key={index}>{result}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+</div>
+  );
+}
+
+export default App;
